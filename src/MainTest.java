1aa13963928897431e6febc765ed22eb6947224e
Sofiane Farkhani <sofiane.farkhani.23150@gmail.com>
2019-11-13 13:34:01 +0100

    Ajout de la fenêtre de Slick

--- src/MainTest.java
+++ src/MainTest.java
@@ -1,9 +1,46 @@
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
 
-public class MainTest {
+public class MainTest extends BasicGame{
 
-	public static void main(String[] args) {
+	public MainTest(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void render(GameContainer gc, Graphics g) throws SlickException {
+		// TODO Auto-generated method stub
 		
-		System.out.println("Hello world");
+	}
+
+	@Override
+	public void init(GameContainer gc) throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(GameContainer gc, int delta) throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public static void main(String[] args) {
+	
+		AppGameContainer app;
+		try {
+			app = new AppGameContainer(new MainTest("FaceToFace"));
+			app.setDisplayMode(800, 600, false);
+			app.setShowFPS(false);
+			app.setTargetFrameRate(60);
+			app.start();
+		} catch (SlickException e) {
+			e.printStackTrace();
+		}
 
 	}
 
