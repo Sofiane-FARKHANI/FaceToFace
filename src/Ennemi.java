3722d0a686213d5b04b320b7209a60ba79f11ac1
Sofiane Farkhani <sofiane.farkhani.23150@gmail.com>
2019-11-27 11:50:52 +0100

    Refactoring du code de la classe Ennemi.

--- src/Ennemi.java
+++ src/Ennemi.java
@@ -6,13 +6,22 @@ import org.newdawn.slick.Graphics;
  *
  */
 public abstract class Ennemi {
+	
 	protected String lettres;
 	protected float x, y, vx; 
 	
+	/**
+	 * Methode permettant de recuperer le mot d un ennemi
+	 * @return : retourne le mot correspondant a l ennemi
+	 */
 	public String getLettres() {
 		return this.lettres;
 	}
 	
+	/**
+	 * Methode permettant de recuperer la coordonnee x de l ennemi
+	 * @return : valeur de la coordonnee x de l ennemi
+	 */
 	public float getX() {
 		return this.x;
 	}
