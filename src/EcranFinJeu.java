123007cb60f863707e2d4481c3136f4bd96485d3
Sofiane Farkhani <sofiane.farkhani.23150@gmail.com>
2019-12-02 14:19:03 +0100

    Mise à jour de l'affichage des boutons.

--- src/EcranFinJeu.java
+++ src/EcranFinJeu.java
@@ -1,13 +1,15 @@
+import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
+import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.gui.AbstractComponent;
 import org.newdawn.slick.gui.ComponentListener;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 
-public class EcranFinJeu extends BasicGameState implements ComponentListener {
+public class EcranFinJeu extends BasicGameState {
 	
 	// Le stateID est de 3 lors du fonctionnement
 	private int stateID=-1, delai;
@@ -20,27 +22,24 @@ public class EcranFinJeu extends BasicGameState implements ComponentListener {
 	@Override
 	public void init(GameContainer gc, StateBasedGame sbg) throws SlickException {
 		
-		this.gameOver=new Image("res/game-over-screen.png").getScaledCopy(800, 700);
+		this.gameOver=new Image("res/game-over-screen.png").getScaledCopy(600, 700);
 		this.delai=0;
 
 	}
 
 	@Override
 	public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException {
-		g.drawImage(gameOver,0,0);
+		g.setBackground(Color.black);
+		g.drawImage(gameOver,100,0);
+		g.setColor(Color.white);
+		g.drawString("PRESS ENTER TO CONTINUE", 300, 600);
 	}
 
 	@Override
 	public void update(GameContainer gc, StateBasedGame sbg, int delta) throws SlickException {
-		delai+=delta;
-		if(delai>=6000)
+		if(gc.getInput().isKeyPressed(Input.KEY_ENTER)) {
 			sbg.enterState(0);
-	}
-
-	@Override
-	public void componentActivated(AbstractComponent ac) {
-		// TODO Auto-generated method stub
-
+		}	
 	}
 
 	@Override
